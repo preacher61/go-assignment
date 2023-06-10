f15ece49ff2185109620e880ef5bd78828393809
shubhamSingh-sib <shubham.singh@sendinblue.com>
2023-06-11 00:39:49 +0530

    adds model package with type representing response

---
+++ model/activity.go
@@ -0,0 +1,8 @@
+package model
+
+// ActivityAPIResposne represents the response object
+// of the activity api.
+type ActivityAPIResposne struct {
+	Activity string `json:"activity"`
+	Key      string `json:"key"`
+}
